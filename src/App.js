8ec7dc7e836157bb5fbdc8363565bfc27aff8c9a
Samuel Diriba <samediriba54@gmail.com>
2024-08-16 04:18:09 +0300

    Delete src directory
    i delete this src folder because there is others.

--- src/App.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const App = express();
-const PORT = process.env.PORT || 5000;
-
-App.listen(PORT, ()=>{
-    console.log(`Server is running on: http://localhost:${PORT}`);
-})
